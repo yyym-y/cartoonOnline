c8d1155d6c406d24083b5e9373242e2c508df9d9
yyym-y <yinmingyang89@gmail.com>
2024-05-10 19:41:12 +0800

    <feat>(./back) add SpringBoot ori code

---
+++ back/src/main/java/org/yyym/back/BackApplication.java
@@ -0,0 +1,13 @@
+package org.yyym.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackApplication.class, args);
+	}
+
+}
