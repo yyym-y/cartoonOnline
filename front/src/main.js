b2c00d121d2cfd281f9d0eb43f8493e474c2bf61
yyym-y <yinmingyang89@gmail.com>
2024-05-11 10:49:58 +0800

    <refactor>(./front) refactor VUE CLI

--- front/src/main.js
+++ front/src/main.js
@@ -4,8 +4,13 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+// import axios from 'axios'
+
+// Vue.prototype.$axios = axios
+
 
 Vue.config.productionTip = false
+
 
 Vue.use(ElementUI)
 
